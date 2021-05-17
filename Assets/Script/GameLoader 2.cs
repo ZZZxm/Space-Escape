39b18d2ff5aaa86cabebec2f707ff800aa52467b
Kerr <kerr99801@gmail.com>
2021-05-17 10:17:14 +0800

    add player controller

---
+++ Assets/Script/GameLoader 2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour
+{
+    public GameObject journeyManager;
+   
+    void Awake()
+    {
+        if(JourneyManager.instance==null)
+        {
+            Instantiate(journeyManager);
+        }
+    }
+}
